ba25f597cd7327568eac460037ecf43165696a25
mnezmah <mario.nezmah@gmail.com>
2019-06-29 18:04:21 +0200

    created auth /router

--- auth/router.js
+++ auth/router.js
@@ -1,5 +1,28 @@
 const { Router } = require('express')
+const { toJWT, toData } = require('./jwt')
 
-const router =  new Router()
+const router = new Router()
+
+router
+  .post(
+    '/logins',
+    (req, res) => {
+      const email = req.body.email
+      const password = req.body.password
+      if (!email || !password) {
+        res.status(400).send({
+          message: 'Please supply a valid email and password'
+        })
+      } else {
+        res.send({ jwt: toJWT({ userId: 1 }) })
+      }
+    })
+  .catch(err => {
+    console.error(err)
+    res.status(500)
+      .send({
+        message: 'something wen wrong'
+      })
+  })
 
 module.exports = router
