94980bd616d755564e55f8b7983bd678fdec4b35
mnezmah <mario.nezmah@gmail.com>
2019-06-29 08:29:34 +0200

    imported playlists in index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 
+const playlists = require('./playlists/model')
+
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log (`listening on port ${port}`))
