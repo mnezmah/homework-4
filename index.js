fd49da1db9b55e34e5515e97c22b80eb5fb2c61d
mnezmah <mario.nezmah@gmail.com>
2019-06-29 18:58:47 +0200

    imported userRouter in index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const playlistsRouter = require('./playlists/router')
 const songsRouter = require('./songs/router') 
 const authRouter = require('./auth/router')
+const userRouter = require('./users/router')
 
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(jsonParser)
 app.use(playlistsRouter)
 app.use(songsRouter)
 app.use(authRouter)
+app.use(userRouter)
 
 
 const port = process.env.PORT || 4000
