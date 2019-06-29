5bc3d344119bf9c5447ab692036cc902c2417f68
mnezmah <mario.nezmah@gmail.com>
2019-06-29 08:44:36 +0200

    imported bodyPaerses in index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 const db = require('./db')
-
 const playlists = require('./playlists/model')
 const playlistsRouter = require('./playlists/router')
+
+const app = express()
+const jsonParser = bodyParser.json()
+
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log (`listening on port ${port}`))
