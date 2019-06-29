baa7f091a4ef1d487abfc53eacedae6839bcd78f
mnezmah <mario.nezmah@gmail.com>
2019-06-29 18:56:31 +0200

    created user router & installed bcrypt

---
+++ users/router.js
@@ -0,0 +1,27 @@
+const { Router } = require('express')
+const User = require('./model')
+const bcrypt = require('bcrypt')
+
+const router = new Router()
+
+router
+  .post(
+    '/user',
+    (req, res, next) => {
+      const user = {
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10)
+      }
+
+      User
+        .create(user)
+        .then(user => {
+          res
+            .status(200)
+            .send(user)
+        })
+        .catch(err => next(err))
+    }
+  )
+
+module.exports = router
